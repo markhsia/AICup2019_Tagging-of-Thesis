5d9f5037d77309a238334355921771dfef0b293a
CubicPower <55009831+markhsia@users.noreply.github.com>
2021-12-23 13:22:16 +0800

    Update example_net.py

--- src/modules/example_net.py
+++ src/modules/example_net.py
@@ -138,7 +138,7 @@ class BertNet(torch.nn.Module):
         print(self.device)
 
 
-    def forward(self, context, context_lens, sentence_token,padding_mask):
+    def forward(self, context, context_lens, sentence_token,token_type_ids=None, attention_mask=None, labels=None):
         batch_size = context.size()[0]
         #max_context_len = context.size()[1]
         max_context_len = 500
@@ -150,6 +150,6 @@ class BertNet(torch.nn.Module):
         print(padding_mask)
         #padding_mask = torch.Tensor(padding_mask).to(self.device)
         
-        answer_prob = self.model(context, context_lens, sentence_token, attention_mask=padding_mask)
+        answer_prob = self.model(context, context_lens, sentence_token)
         #answer_prob = self.sigmoid(answer_prob)
         return answer_prob
